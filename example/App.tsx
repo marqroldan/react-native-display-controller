036589cd212c70e186a43c356763be1b6be703b1
Marq Roldan <hcmlroldan@gmail.com>
2024-07-27 09:11:29 +0800

    added navigation package

--- example/App.tsx
+++ example/App.tsx
@@ -1,12 +1,14 @@
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 
 import * as RNOverlay from '@marqroldan/react-native-overlay';
 
 export default function App() {
-  return (
+  return (<NavigationContainer>
     <View style={styles.container}>
       <Text><RNOverlay.Hello/></Text>
     </View>
+  </NavigationContainer>
   );
 }
 
