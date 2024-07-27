a67570d61c31ea4444fd8126b3a9a916377ccd30
Marq Roldan <hcmlroldan@gmail.com>
2024-07-27 09:16:58 +0800

    added native stack

--- example/App.tsx
+++ example/App.tsx
@@ -1,14 +1,28 @@
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import * as RNOverlay from '@marqroldan/react-native-overlay';
 
-export default function App() {
-  return (<NavigationContainer>
-    <View style={styles.container}>
-      <Text><RNOverlay.Hello/></Text>
-    </View>
-  </NavigationContainer>
+function HomeScreen() {
+  return (
+      <View style={styles.container}>
+        <Text>Home Screen</Text>
+      </View>
+  );
+}
+
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+      <View style={styles.container}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeScreen} />
+        </Stack.Navigator>
+      </NavigationContainer></View>
   );
 }
 
