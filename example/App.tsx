2edef9009bdc327e7e89b423d4616284771397e8
Marq Roldan <hcmlroldan@gmail.com>
2024-07-29 08:41:33 +0800

    stashing stuff

--- example/App.tsx
+++ example/App.tsx
@@ -1,34 +1,60 @@
-import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import {Overlay} from '@marqroldan/react-native-overlay';
+import * as RNOverlay from "@marqroldan/react-native-overlay";
 
 function HomeScreen() {
   return (
-      <View style={styles.container}>
+    <View style={styles.container}>
+      <View style={{ flex: 1 }}>
         <Text>Home Screen</Text>
       </View>
+      <View
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          right: 0,
+          bottom: 0,
+          backgroundColor: "rgba(255,0,0,0.1)",
+        }}
+      >
+        <Text>Overlay Screen</Text>
+      </View>
+    </View>
   );
 }
-
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-      <View style={styles.container}>
+    <View style={styles.container}>
       <NavigationContainer>
         <Stack.Navigator>
           <Stack.Screen name="Home" component={HomeScreen} />
         </Stack.Navigator>
-      </NavigationContainer></View>
+      </NavigationContainer>
+      <View
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          right: 0,
+          bottom: 0,
+          backgroundColor: "rgba(0,255,0,0.3)",
+        }}
+      >
+        <Text>Overlay Screen</Text>
+      </View>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
   },
 });
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,75 @@
-import React from 'react';
+import type { PropsWithChildren, ReactNode, ComponentClass } from "react";
+import React, { useState, createContext, useEffect, useMemo } from "react";
 
-type Props = React.PropsWithChildren<{
+const StackContext = createContext([]);
+const StackScrimContext = createContext(false);
+const OverlayActionsContext = createContext({
+  registerSelf: (name: string, listener: () => void) => {},
+  show: (target: string, data: any) => {},
+});
 
-}>
+type HolderProps = {
+  name: string;
+  ScrimComponent: (() => ReactNode) | ComponentClass<any>;
+};
+function Holder(props: PropsWithChildren<HolderProps>) {
+  const { registerSelf } = useOverlayController();
+  const [stack, setStack] = useState([]);
 
-export class Overlay extends React.Component<Props, {}> {
-  render() {
-    return <>
-      {this.props.children}
-    </>
-  }
+  useEffect(() => {
+    return registerSelf(props.name, (data) => {
+      setStack((prevStack) => {
+        const newStack = prevStack.slice();
+
+        newStack.push(data);
+        return newStack;
+      });
+    });
+  }, []);
+
+  const shouldShowStackScrim = stack.some((a) => a.showStackScrim);
+
+  return (
+    <StackScrimContext.Provider value={shouldShowStackScrim}>
+      <StackContext.Provider value={stack}>
+        {props.children}
+      </StackContext.Provider>
+    </StackScrimContext.Provider>
+  );
+}
+
+function OverlayController(props) {
+  const [holders, setHolders] = useState({});
+  const [data, setData] = useState({});
+
+  const actions = useMemo(() => {
+    return {
+      registerSelf: (name, listener) => {
+        return () => {};
+      },
+      show: (target: string, data: any) => {
+        setData((prevData) => {
+          const newData = {
+            ...prevData,
+          };
+
+          if (!newData[target]) {
+            newData[target] = [];
+          }
+
+          newData[target].push(data);
+        });
+      },
+    };
+  }, []);
+
+  useEffect(() => {
+    //// check the data and see if there's a listener, filter if it's not meant to fallback
+  }, [data]);
+
+  return (
+    <OverlayActionsContext.Provider value={actions}>
+      {props.children}
+    </OverlayActionsContext.Provider>
+  );
 }
